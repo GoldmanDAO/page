6c510a03a27704b70440a2213247c6abf027b7d6
xPi2 <xPi2@users.noreply.github.com>
2022-03-14 13:25:42 +0000

    deploy: 3e631c661a8043dc4f2e389ed903c027c8188dd1

---
+++ _next/static/_hterqJS-G-EfJp664xfm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1598a7e3da855a80.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
